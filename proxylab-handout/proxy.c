81593281f5ac583293ebb3e7b409f002fd341644
Wei Chen <cleverdie@gmail.com>
2013-07-23 16:31:34 +0800

    proxy init code

---
+++ proxylab-handout/proxy.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#define MAX_CACHE_SIZE 1049000
+#define MAX_OBJECT_SIZE 102400
+
+static const char *user_agent = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n";
+static const char *accept = "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\n";
+static const char *accept_encoding = "Accept-Encoding: gzip, deflate\r\n";
+
+int main()
+{
+    printf("%s%s%s", user_agent, accept, accept_encoding);
+
+    return 0;
+}
